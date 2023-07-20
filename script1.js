a1f3ea8b033ee39af50d496013db50a9460b8abd
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-20 10:10:43 +1000

    Initial commit

---
+++ script1.js
@@ -0,0 +1,24 @@
+// Create a variable which stores all character data
+
+const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+
+// Set password length - develop this to let user select length
+
+// Grab the output HTML elements
+
+// Create a function that will generate a random password containing number characters from passwordLength
+
+function generate() {
+  // Declare a variable "password and assign an empty string to it's value"
+}
+
+// Creates a function outputPasswords() that adds the generated passwords to the HTML elements when clicking Generate Passwords <button>
+
+function outputPasswords() {
+  // Display the output in the HTML outputDisplay
+}
+
+// Add a function that will copy the text on click
+
+// Invoke function
+// console.log(generate());
